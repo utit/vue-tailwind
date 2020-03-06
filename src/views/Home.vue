311202d16514badf795f2c757ea964f9693367ea
utit <triyaid@gmail.com>
2020-03-06 10:54:56 +0700

    start

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="mx-auto h-full flex justify-center items-center">
+      <img class="" alt="Vue logo" src="../assets/logo.png">  
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
