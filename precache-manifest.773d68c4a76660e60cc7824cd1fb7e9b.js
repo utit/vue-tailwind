12c074e20ea6d01d1bb8f9df8a0760ea7b6583a2
utit <triyaid@gmail.com>
2020-03-07 08:31:28 +0700

    deploy

---
+++ precache-manifest.773d68c4a76660e60cc7824cd1fb7e9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8f6219ccccda61d7b6",
+    "url": "/vue-tailwind/css/app.3b7d8387.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-tailwind/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "92cb2a91570ee27aba311f8d8a784f5a",
+    "url": "/vue-tailwind/index.html"
+  },
+  {
+    "revision": "1eb5ecbd494a00399dc5",
+    "url": "/vue-tailwind/js/about.c2d6801a.js"
+  },
+  {
+    "revision": "7e8f6219ccccda61d7b6",
+    "url": "/vue-tailwind/js/app.61093e72.js"
+  },
+  {
+    "revision": "2a96280f0d71dafabaa7",
+    "url": "/vue-tailwind/js/chunk-vendors.b9f7f6c4.js"
+  },
+  {
+    "revision": "e08a57e7bbc3fab4a5c5036bde4b1f25",
+    "url": "/vue-tailwind/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-tailwind/robots.txt"
+  }
+]);
